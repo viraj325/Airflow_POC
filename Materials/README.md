c3753fa2de9d86576ab1db3d239756d094f7271e
Viraj Patel <virajpatel325@gmail.com>
2024-09-08 23:16:02 -0400

    update

--- Materials/README.md
+++ Materials/README.md
@@ -3,5 +3,12 @@ Fill it in...
 
 # Helpful Commands
 * Make sure the path is in Materials
-- docker-compose ps
 - docker-compose up -d
+    - https://localhost:8080
+- Troubleshoot
+    - docker-compose ps
+    - docker logs materials_name_of_the_container
+- docker-compose down
+    - To exit
+- docker volume prune
+    - ![alt text](https://img-c.udemycdn.com/redactor/raw/article_lecture/2023-03-06_08-28-42-9884e885f01d16f91bd4a0f03ed267db.png)
--- Materials/dags/test_dag.py
+++ Materials/dags/test_dag.py
@@ -15,27 +15,24 @@ k = KubernetesPodOperator(
     do_xcom_push=True,
 )
 
-quay_k8s = KubernetesPodOperator(
-    namespace="default",
-    image="quay.io/apache/bash",
-    image_pull_secrets=[k8s.V1LocalObjectReference("testquay")],
-    cmds=["bash", "-cx"],
-    arguments=["echo", "10", "echo pwd"],
-    labels={"foo": "bar"},
-    name="airflow-private-image-pod",
-    on_finish_action="delete_pod",
-    in_cluster=True,
-    task_id="task-two",
-    get_logs=True,
-)
-
-def testK8() -> bool:
-    k.dry_run()
-    return True
-
 with DAG(dag_id="test_dag",
          start_date=datetime(2023,10,11),
          schedule_interval=timedelta(hours=24),
          catchup=False) as dag:
     
-    testK8()
+    k.dry_run()
+
+
+# example_k8_operator = KubernetesPodOperator(
+#     namespace="default",
+#     image="quay.io/apache/bash",
+#     image_pull_secrets=[k8s.V1LocalObjectReference("testquay")],
+#     cmds=["bash", "-cx"],
+#     arguments=["echo", "10", "echo pwd"],
+#     labels={"foo": "bar"},
+#     name="airflow-private-image-pod",
+#     on_finish_action="delete_pod",
+#     in_cluster=True,
+#     task_id="task-two",
+#     get_logs=True,
+# )
